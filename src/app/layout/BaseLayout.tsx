810d292e005a7fd9f9fdb3db591a364a7dad4859
Vsevo;od <96574851+vsevolod22@users.noreply.github.com>
2025-05-06 20:30:53 +0300

    Update BaseLayout.tsx

--- src/app/layout/BaseLayout.tsx
+++ src/app/layout/BaseLayout.tsx
@@ -11,7 +11,7 @@ const BaseLayout = () => {
 
    return (
       <>
-         {accessToken ? (
+         
             <div className='flex min-h-screen flex-col bg-background'>
                <Header />
                <main className='flex-1 py-8'>
@@ -19,29 +19,7 @@ const BaseLayout = () => {
                </main>
                <Footer />
             </div>
-         ) : (
-            <div className='relative flex h-screen items-center justify-center gap-3 bg-background px-4 sm:px-6 lg:px-8'>
-               <ThemeSwitcher className='absolute right-4 top-4' />
-               <img
-                  alt='Изображение для стартовой страницы'
-                  className='hidden h-[800px] w-[800px] md:h-[500px] md:w-[500px] lg:block lg:h-[600px]'
-                  src={NonAuthImage}
-               />
-               <div className='flex max-w-[700px] flex-col gap-8 text-center lg:gap-16'>
-                  <p className='text-xl font-bold md:text-2xl lg:text-3xl'>
-                     Присоединяйся к тем, кто уже живет с удовольствием, управляй задачами, анализируй нагрузку и наслаждайся
-                     успехом!
-                  </p>
-                  <div className='flex flex-col items-center gap-3 md:flex-row'>
-                     <AuthModal className='h-12 w-48 bg-primary text-base font-bold uppercase hover:bg-primary-hover md:h-[56px] md:w-56 md:text-xl' />
-                     <AuthModal
-                        authModalMode='register'
-                        className='h-12 w-48 border border-primary text-base font-bold uppercase md:h-[56px] md:w-56 md:text-xl'
-                     />
-                  </div>
-               </div>
-            </div>
-         )}
+         
       </>
    );
 };
